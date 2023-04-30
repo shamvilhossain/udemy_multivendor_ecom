400a1e60a457073ea6a75c8daffea9fc1d1d9725
unknown <shamvil89@gmail.com>
2023-05-01 01:05:55 +0600

    admin login form

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -10,4 +11,19 @@ class AdminController extends Controller
     { 
         return view('admin.index');
     }
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    } // End Mehtod 
+
+
+    public function AdminDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } // End Mehtod
 }
