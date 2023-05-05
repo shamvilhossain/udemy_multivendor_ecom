b324c8749a7c2d9b683fcb1371a7b8608e1b00f7
unknown <shamvil89@gmail.com>
2023-05-06 02:35:20 +0600

    vendor change password

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -65,4 +65,31 @@ class VendorController extends Controller
         return redirect()->back()->with($notification);
 
     } // End Mehtod 
+
+    public function VendorChangePassword(){
+        return view('vendor.vendor_change_password');
+    } // End Mehtod 
+
+
+
+    public function VendorUpdatePassword(Request $request){
+        // Validation 
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed', 
+        ]);
+
+        // Match The Old Password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Doesn't Match!!");
+        }
+
+        // Update The new password 
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+
+        ]);
+        return back()->with("status", " Password Changed Successfully");
+
+    } // End Mehtod 
 }
