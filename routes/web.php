c5b9baf4fa595f3e21a674c261e9f4b9b18a0521
unknown <shamvil89@gmail.com>
2023-06-24 21:30:50 +0600

    product form design

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Controllers\Backend\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -112,6 +113,14 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::post('/active/vendor/approve' , 'ActiveVendorApprove')->name('active.vendor.approve');
         Route::get('/active/vendor/details/{id}' , 'ActiveVendorDetails')->name('active.vendor.details');
         Route::post('/inactive/vendor/approve' , 'InActiveVendorApprove')->name('inactive.vendor.approve');
+
+    });
+
+     // Product All Route 
+    Route::controller(ProductController::class)->group(function(){
+        Route::get('/all/product' , 'AllProduct')->name('all.product');
+        Route::get('/add/product' , 'AddProduct')->name('add.product');
+
 
     });
    
