555528e2f11b2670cdf0e84a673ad5fa7926c9d4
unknown <shamvil89@gmail.com>
2023-06-28 13:09:20 +0600

    product update into db

--- routes/web.php
+++ routes/web.php
@@ -122,6 +122,8 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::get('/all/product' , 'AllProduct')->name('all.product');
         Route::get('/add/product' , 'AddProduct')->name('add.product');
         Route::post('/store/product' , 'StoreProduct')->name('store.product');
+        Route::get('/edit/product/{id}' , 'EditProduct')->name('edit.product');
+        Route::post('/update/product' , 'UpdateProduct')->name('update.product');
 
     });
    
