fb56f25ca71b6d14571380e8065465885c9a2b94
unknown <shamvil89@gmail.com>
2023-07-07 15:33:59 +0600

    vendor product part 2

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,8 @@ Route::middleware(['auth','role:vendor'])->group(function () {
     // Vendor Add Product All Route 
     Route::controller(VendorProductController::class)->group(function(){
         Route::get('/vendor/all/product' , 'VendorAllProduct')->name('vendor.all.product');
-
+        Route::get('/vendor/add/product' , 'VendorAddProduct')->name('vendor.add.product');
+        Route::get('/vendor/subcategory/ajax/{category_id}' , 'VendorGetSubCategory');
 
     });
 });
