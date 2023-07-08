c15d268bb584c0d9db0e72d0638481f620de94b7
unknown <shamvil89@gmail.com>
2023-07-08 22:05:58 +0600

    slider crud

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\Backend\VendorProductController;
+use App\Http\Controllers\Backend\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -157,6 +158,17 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::get('/product/inactive/{id}' , 'ProductInactive')->name('product.inactive');
         Route::get('/product/active/{id}' , 'ProductActive')->name('product.active');
         Route::get('/delete/product/{id}' , 'ProductDelete')->name('delete.product');
+
+    });
+
+    // Slider All Route 
+    Route::controller(SliderController::class)->group(function(){
+        Route::get('/all/slider' , 'AllSlider')->name('all.slider');
+        Route::get('/add/slider' , 'AddSlider')->name('add.slider');
+        Route::post('/store/slider' , 'StoreSlider')->name('store.slider');
+        Route::get('/edit/slider/{id}' , 'EditSlider')->name('edit.slider');
+        Route::post('/update/slider' , 'UpdateSlider')->name('update.slider');
+        Route::get('/delete/slider/{id}' , 'DeleteSlider')->name('delete.slider');
 
     });
    
