d7666492ee7435531bfb5b424c32af00be2577d8
unknown <shamvil89@gmail.com>
2023-09-01 21:28:56 +0600

    bug fix

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -16,6 +16,10 @@ class CartController extends Controller
 {
     public function AddToCart(Request $request, $id){
 
+        if(Session::has('coupon')){
+            Session::forget('coupon');
+        }
+
         $product = Product::findOrFail($id);
 
         if ($product->discount_price == NULL) {
@@ -79,6 +83,10 @@ class CartController extends Controller
     }// End Method
 
     public function AddToCartDetails(Request $request, $id){
+
+        if(Session::has('coupon')){
+            Session::forget('coupon');
+        }
 
         $product = Product::findOrFail($id);
 
@@ -145,6 +153,19 @@ class CartController extends Controller
 
     public function CartRemove($rowId){
         Cart::remove($rowId);
+
+        if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
+        
         return response()->json(['success' => 'Successfully Remove From Cart']);
 
     }// End Method
