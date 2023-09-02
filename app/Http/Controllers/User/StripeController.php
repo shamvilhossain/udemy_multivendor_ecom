cc1f8e0785cb474aeb521b5025ec589c801126fa
unknown <shamvil89@gmail.com>
2023-09-02 16:16:27 +0600

    order tables added

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -7,7 +7,24 @@ use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    public function StripeOrder(){
+    public function StripeOrder(Request $request){
+        // Set your secret key. Remember to switch to your live secret key in production.
+        // See your keys here: https://dashboard.stripe.com/apikeys
+        \Stripe\Stripe::setApiKey('sk_test_51GyVvWIu0VuBnOuuzrRxXmkhFhdAORprp41OAbe8AHa0PJAVtRs7v6EA6I5bzi4NfEdh4Fw7Fq0swV50W9Lo525M00ygPynIdl');
+
+        // Token is created using Checkout or Elements!
+        // Get the payment token ID submitted by the form:
+        $token = $_POST['stripeToken'];
+
+        $charge = \Stripe\Charge::create([
+            'amount' => 999*100,
+            'currency' => 'usd',
+            'description' => 'Example charge',
+            'source' => $token,
+            'metadata' => ['order_id' => '6735'],
+        ]);
+
+        dd($charge);
 
 
     }// End Method 
--- resources/views/frontend/payment/stripe.blade.php
+++ resources/views/frontend/payment/stripe.blade.php
@@ -155,7 +155,7 @@
     
     <script type="text/javascript">
         // Create a Stripe client.
-    var stripe = Stripe('pk_test_zuURZYgtzc5QCrAq3ITN7h2M007nb4GJy9');
+    var stripe = Stripe('pk_test_51GyVvWIu0VuBnOuuV89bTJEceb6ye8K3SfnieLAsEXRqXwvhYzAJKiNHXkGscco7sNa91o59PNvZCu4cMT7QdyNC005xBPL0ny');
     // Create an instance of Elements.
     var elements = stripe.elements();
     // Custom styling can be passed to options when creating an Element.
