2b626d2bd5a21784d81ace3cd8bcc0960d449ebe
unknown <shamvil89@gmail.com>
2023-11-06 20:21:58 +0600

    install laravel spatie

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
